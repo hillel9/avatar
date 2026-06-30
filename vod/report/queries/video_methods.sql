a2a2321fc5b0628d46f21dda6b4effce20243c3b
Hillel <hillel.cohen@kaltura.com>
2026-06-30 07:22:02 +0300

    g

--- vod/report/queries/video_methods.sql
+++ vod/report/queries/video_methods.sql
@@ -7,5 +7,6 @@ INNER JOIN nvp1.ovp_raw.partner ON partnerId = Id
 WHERE partition_date >= CURRENT_DATE - INTERVAL '31 days'
 AND kalturaApplication = 'Avatar Videos'
 AND eventVar2 = 'Select avatar video type'
+AND nvp1.ovp_raw.application_events.partnerId IN (2167581,2189801,3193793,4170533,2314871,1714341,1889041,2167551,1315742,21428242,5877472,2213002,2503612,4733002,1038472,2213002,1428242,5644772,1921661)
 
 GROUP BY eventVar3
